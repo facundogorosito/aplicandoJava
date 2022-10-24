cbd6b2ce511517b1eac3e94d639323c0eb96ab2c
facundogorosito <facundoignaciogorosito@gmail.com>
2022-10-24 19:12:40 -0300

    se agrega proyecto java

---
+++ scrip.js
@@ -0,0 +1,22 @@
+console.log("Bienvenidos a JavaScrip")
+
+//TRABAJA DE FORMA STRING POR DEFECTO
+let nombre = prompt("Ingrese su nombre")
+
+console.log("El usuario a ingresado: " + nombre)
+
+alert("Hola !" + " " + nombre + "Tu novio te ama")
+
+
+//TRABAJA DE FORMA NUMBER CAMBIANDOLO
+
+let numero1 = Number (prompt("Ingrese un numero"))
+
+console.log(numero1)
+
+//CONS.LOG SUMANDOSE
+
+let numero2 = Number (prompt("Ingrese otro numero"))
+
+console.log(numero1 + numero2 + (numero2 + numero2))
+
