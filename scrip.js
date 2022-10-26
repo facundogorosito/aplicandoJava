b435f9db5b4cec1ea4d4ce1c9c03cc1d105f64a4
facundogorosito <facundoignaciogorosito@gmail.com>
2022-10-26 20:34:22 -0300

    modifico js

--- scrip.js
+++ scrip.js
@@ -1,5 +1,5 @@
 function saludar() {
-    console.log("Hola bienvenido a Ataque mortal!");
+    console.log("Hola bienvenido a Raquetas FIG!");
 }
 saludar();
 
@@ -8,69 +8,95 @@ let nombre = prompt("Ingrese su nombre");
 
 console.log("El usuario a ingresado: " + nombre);
 
-alert("Hola !" + " " + nombre + " Vamos a divertinos un poco!");
+alert("Hola !" + " " + nombre + " Somos Fanaticos del tenis y vendemos productos relacionados a ello.");
 
-let ataque = prompt("Deseas ganar cueste lo que cueste ¿si o no?");
+let productos;
+let rafaNadal = 90000;
+let rogerFederer = 105000;
+let djoko = 85000;
+let ropaNadal = 70000;
+let ropaFederer = 80000;
+let ropaDjoko = 75000;
+let acceNadal = 20000;
+let acceFederer = 25000;
+let acceDjioko = 23000
+let totalCompra = 0;
 
-while (ataque != "si" && ataque != "no") {
-    alert("Por favor ingresar si o no");
-    ataque = prompt("Hola, tienes ganas de ganar ¿si o no?");
-}
-if (ataque == "si") {
-    alert("A continuación combatiremos");
-    let ataque = prompt("Escribe 0 para el primer ataque, 1 para segundo ataque, 2 para el tercer ataque y 3 para el cuarto ataque");
-    if (ataque == 0) {
-        alert("Tiene un valor de " + 80 + " de daño")
-    } else if (ataque == 1) {
-        alert("Tiene un valor de " + 20 + " de daño")
-    } else if (ataque == 2) {
-        alert("Tiene un valor de " + 15 + " de daño")
-    } else if (ataque == 3) {
-        alert("Tiene un valor de " + 10 + " de daño")
-    } else {
-        alert("No es la opcion correcta")
-    }
-} else if (ataque == "no") {
-    alert("Gracias por visitarnos, hasta pronto!!")
-}
 
-let primerAtac = 80;
-let segundoAtac = 20;
-let tercerAtac = 15;
-let cuartoAtac = 100;
-let totalAtac = 0;
-
-while (ataque != "no") {
-    let producto = prompt("eligue otro ataque");
-    let daño = 0;
-
-    if (producto == primerAtac || producto == cuartoAtac) {
-        switch (producto) {
-            case primerAtac:
-                daño = 80;
-                alert("ganaste!!!")
-                break
-            case segundoAtac:
-                daño = 20;
-                alert("perdiste :(")
-                break
-            case tercerAtac:
-                daño = 15;
-                alert ("perdiste :(")
-                break
-            case cuartoAtac:
-                daño = 100;
-                alert ("has derrotado por completo")
-                break
-            default: 
-            break;
+function raquetas(){
+        productos = parseInt(prompt("Para comprar raqueta de Rafael Nadal ingrese 1, para comprar de Roger Federer ingrese 2 o para comprar de Djokovik ingrese 3"));
+        if(productos==1){
+            totalCompra = totalCompra + rafaNadal
+            console.log("compro raqueta de Nadal")
+        }else if (productos==2){
+            totalCompra = totalCompra + rogerFederer
+            console.log("compro raqueta de Roger")
+        }else if (productos==3){
+            totalCompra = totalCompra + djoko
+            console.log("compro raqueta de Djoko")
+        }else{
+            alert("No es la opcion que solicitamos")
         }
-    }else {
-        alert ("Te quedaste sin ataques")
     }
-    ataque = prompt ("Desea elegir?")
-    while (ataque === "no"){
-        alert ("Gracias por jugar con nosotros, Hasta pronto!")
-        break;
+
+    function ropa(){
+        productos = parseInt(prompt("Para comprar ropa de Rafael Nadal ingrese 1, para comprar de Roger Federer ingrese 2 o para comprar de Djokovik ingrese 3"));
+        if(productos==1){
+            totalCompra = totalCompra + ropaNadal
+            console.log("compro ropa de Nadal")
+        }else if (productos==2){
+            totalCompra = totalCompra + ropaFederer
+            console.log("compro ropa de Federer")
+        }else if (productos==3){
+            totalCompra = totalCompra + ropaDjoko
+            console.log("compro ropa de Djokovik")
+        }else{
+            alert("No es la opcion que solicitamos")
+        }
     }
-}
+
+    function accesorios(){
+        productos = parseInt(prompt("Para comprar accesorios de Rafael Nadal ingrese 1, para comprar de Roger Federer ingrese 2 o para comprar de Djokovik ingrese 3"));
+        if(productos==1){
+            totalCompra = totalCompra + acceNadal
+            console.log("compro accesorios de nadal")
+        }else if (productos==2){
+            totalCompra = totalCompra + acceFederer
+            console.log("compro accesorios de Federer")
+        }else if (productos==3){
+            totalCompra = totalCompra + acceDjioko
+            console.log("compro accesorios de Djokovik")
+        }else{
+            alert("No es la opcion que solicitamos")
+        }
+    }
+
+let opcion;
+
+do{
+    opcion = parseInt(prompt("¿Que te interesa comprar? Escribe 1 para raquetas, 2 para ropa, 3 para accesorios o 4 para salir"))
+
+    switch(opcion){
+        case 1:
+            console.log("Comprar Raquetas")
+            raquetas();
+            break
+        case 2:
+            console.log("Comprar Ropa")
+            ropa()
+            break
+        case 3:
+            console.log("Comprar Accesorios")
+            accesorios()
+            break
+        case 4:
+                console.log("salir")
+                break
+        default:
+            console.log("no ha elegido ninguna opcion")
+            alert("no ha elegido ninguna opcion")
+        break
+    }
+}while(opcion!=4)
+
+alert("El valor total de su compra es de: $" + totalCompra)
