d1a1e029d142cd31c8587e9f46dff7dc84172554
facundogorosito <facundoignaciogorosito@gmail.com>
2022-11-08 22:08:03 -0300

    modifico javaS

--- scrip.js
+++ scrip.js
@@ -10,93 +10,83 @@ console.log("El usuario a ingresado: " + nombre);
 
 alert("Hola !" + " " + nombre + " Somos Fanaticos del tenis y vendemos raquetas para tu gusto");
 
-let productos;
-let rafaNadal = 90000;
-let rogerFederer = 105000;
-let djoko = 85000;
-let ropaNadal = 70000;
-let ropaFederer = 80000;
-let ropaDjoko = 75000;
-let acceNadal = 20000;
-let acceFederer = 25000;
-let acceDjioko = 23000
-let totalCompra = 0;
+const raquetas = [
+    {nombre:"wilsonBorn", variedad: "negra", precio: 71599},
+    {nombre:"headGraphene", variedad: "negra", precio: 72990},
+    {nombre:"wilsonProOpen", variedad: "amarilla", precio: 47500},
+    {nombre:"wilsonUltraPower", variedad: "naranja", precio: 41999},
+    {nombre:"headMxCyber", variedad: "negra", precio: 31700},
+    {nombre:"headMxSpark", variedad: "azul", precio: 20800},
+    {nombre:"babolatJunior", variedad: "blanca", precio: 12300},
+    {nombre:"headSpeed", variedad: "negra", precio: 76180},
+];
 
+let carrito = []
 
-function raquetas(){
-        productos = parseInt(prompt("Para comprar raqueta de Rafael Nadal ingrese 1, para comprar de Roger Federer ingrese 2 o para comprar de Djokovik ingrese 3"));
-        if(productos==1){
-            totalCompra = totalCompra + rafaNadal
-            console.log("compro raqueta de Nadal")
-        }else if (productos==2){
-            totalCompra = totalCompra + rogerFederer
-            console.log("compro raqueta de Roger")
-        }else if (productos==3){
-            totalCompra = totalCompra + djoko
-            console.log("compro raqueta de Djoko")
-        }else{
-            alert("No es la opcion que solicitamos")
+let seleccion = prompt("Bievenidos! Deseas comprar alguna raqueta?");
+
+while(seleccion != "si" && seleccion != "no"){
+    alert("Por favor ingrese Si o No")
+    seleccion = prompt("Hola, desear comprar alguna raqueta?")
+}
+
+if(seleccion == "si"){
+    alert("A continuacion nuestra lista de raquetas disponibles")
+    let raquetasDisponibles = raquetas.map((raqueta) => raqueta.nombre + " " + "$" + raqueta.precio);
+         alert(raquetasDisponibles.join(" - "));
+    }else if(seleccion == "no"){
+        alert("Gracias por su visita, lamentamos no poder ayudarte 😢")
+}
+
+while(seleccion != "no"){
+    let raqueta = prompt("Agrega una raqueta a tu carrito")
+    let precio = 0;
+
+    if (raqueta == "wilsonBorn" || raqueta == "headGraphene" || raqueta == "wilsonProOpen" || raqueta == "wilsonUltraPower" || raqueta == "headMxCyber" || raqueta == "headMxSpark" || raqueta == "babolatJunior" || raqueta == "headSpeed"){
+        switch(raqueta){
+            case "wilsonBorn":
+                precio = 71599
+                break;
+             case "headGraphene":
+                precio = 72990
+                break;
+             case "wilsonProOpen":
+                precio = 47500
+                break;
+             case "wilsonUltraPower":
+                precio = 41999
+                break;
+             case "headMxCyber":
+                precio = 31700
+                break; case "headMxSpark":
+                precio = 20800
+                break;
+             case "babolatJunior":
+                precio = 12300
+                break;
+             case "headSpeed":
+                precio = 76180
+                break;
+        
         }
+    let unidades = parseInt(prompt("Cuantas unidades vas a llevar?"))
+
+    carrito.push({raqueta, unidades, precio})
+    console.log(carrito)
+    } else {
+        alert("No tenemos este producto en stock")
     }
 
-    function ropa(){
-        productos = parseInt(prompt("Para comprar ropa de Rafael Nadal ingrese 1, para comprar de Roger Federer ingrese 2 o para comprar de Djokovik ingrese 3"));
-        if(productos==1){
-            totalCompra = totalCompra + ropaNadal
-            console.log("compro ropa de Nadal")
-        }else if (productos==2){
-            totalCompra = totalCompra + ropaFederer
-            console.log("compro ropa de Federer")
-        }else if (productos==3){
-            totalCompra = totalCompra + ropaDjoko
-            console.log("compro ropa de Djokovik")
-        }else{
-            alert("No es la opcion que solicitamos")
-        }
+    seleccion = prompt("Queres seguir comprando?")
+    while (seleccion === "no") {
+        alert("Gracias por la compra")
+        carrito.forEach((carritoFinal) => {
+            console.log(`producto: ${carritoFinal.raqueta}, unidades: ${carritoFinal.unidades}, Total a pagar por producto: ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+
+    break;
     }
+}
 
-    function accesorios(){
-        productos = parseInt(prompt("Para comprar accesorios de Rafael Nadal ingrese 1, para comprar de Roger Federer ingrese 2 o para comprar de Djokovik ingrese 3"));
-        if(productos==1){
-            totalCompra = totalCompra + acceNadal
-            console.log("compro accesorios de nadal")
-        }else if (productos==2){
-            totalCompra = totalCompra + acceFederer
-            console.log("compro accesorios de Federer")
-        }else if (productos==3){
-            totalCompra = totalCompra + acceDjioko
-            console.log("compro accesorios de Djokovik")
-        }else{
-            alert("No es la opcion que solicitamos")
-        }
-    }
-
-let opcion;
-
-do{
-    opcion = parseInt(prompt("¿Que te interesa comprar? Escribe 1 para raquetas, 2 para ropa, 3 para accesorios o 4 para salir"))
-
-    switch(opcion){
-        case 1:
-            console.log("Comprar Raquetas")
-            raquetas();
-            break
-        case 2:
-            console.log("Comprar Ropa")
-            ropa()
-            break
-        case 3:
-            console.log("Comprar Accesorios")
-            accesorios()
-            break
-        case 4:
-                console.log("salir")
-                break
-        default:
-            console.log("no ha elegido ninguna opcion")
-            alert("no ha elegido ninguna opcion")
-        break
-    }
-}while(opcion!=4)
-
-alert("El valor total de su compra es de: $" + totalCompra)
+const total = carrito.reduce((acumulador, elem) => acumulador + elem.precio * elem.unidades,0)
+alert(`El total a pagar es de: ${total}`);
